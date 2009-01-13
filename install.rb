9b9d5b39c684033ff859ef9f36297b22a5187307
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2009-01-13 23:39:32 +0800

    showing error in install.rb if database is not supported
    Signed-off-by: Marco Otte-Witte <marco.otte-witte@simplabs.com>

--- install.rb
+++ install.rb
@@ -1,1 +1,13 @@
-puts IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+supported_databases = [
+  'ActiveRecord::ConnectionAdapters::PostgreSQLAdapter',
+  'ActiveRecord::ConnectionAdapters::MysqlAdapter',
+  'ActiveRecord::ConnectionAdapters::SQLite3Adapter'
+]
+
+unless supported_databases.include?(ActiveRecord::Base.connection.class.to_s)
+  puts <<-EOT
+    =====================================
+    Your database #{ActiveRecord::Base.connection.class} is not supported by reports_as_sparkline
+    =====================================
+  EOT
+end

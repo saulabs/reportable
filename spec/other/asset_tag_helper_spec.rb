0501284c2f5ff5aef62c0fc312f9bbfde444239a
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2009-01-14 02:13:57 +0800

    added spec for Kvlr::ReportsAsSparkline::AssetTagHelper#sparkline_tag
    Signed-off-by: Marco Otte-Witte <marco.otte-witte@simplabs.com>

---
+++ spec/other/asset_tag_helper_spec.rb
@@ -0,0 +1,27 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+
+describe Kvlr::ReportsAsSparkline::AssetTagHelper do
+
+  before do
+    @helper = TestHelper.new
+  end
+
+  describe '#sparkline_tag' do
+
+    it 'should render an image with the correct source' do
+      @helper.should_receive(:image_tag).once.with(
+        'http://chart.apis.google.com/chart?cht=ls&chs=300x34&chd=t:1.0,2.0,3.0&chco=0077cc&chm=B,E6F2FA,0,0,0&chls=1,0,0&chds=1.0,3.0'
+      )
+
+      @helper.sparkline_tag([[DateTime.now, 1.0], [DateTime.now, 2.0], [DateTime.now, 3.0]])
+    end
+
+  end
+
+end
+
+class TestHelper
+
+  include Kvlr::ReportsAsSparkline::AssetTagHelper
+
+end

2233c8215611f8dce155dff4597175d8dce8c155
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2009-01-14 00:39:49 +0800

    cleaned up asset tage helper
    Signed-off-by: Marco Otte-Witte <marco.otte-witte@simplabs.com>

--- lib/kvlr/reports_as_sparkline/asset_tag_helper.rb
+++ lib/kvlr/reports_as_sparkline/asset_tag_helper.rb
@@ -20,7 +20,7 @@ module Kvlr #:nodoc:
       # <%= sparkline_tag(User.registrations_report, :width => 200, :height => 100, :color => '000') %>
       def sparkline_tag(data, options = {})
         options.reverse_merge!({:width => 300, :height => 34, :color => '0077cc'})
-        data.collect! { |element| element[1].to_s }
+        data.collect! { |d| d[1] }
         image_tag(
           "http://chart.apis.google.com/chart?cht=ls&chs=#{options[:width]}x#{options[:height]}&chd=t:#{data.join(',')}&chco=#{options[:color]}&chm=B,E6F2FA,0,0,0&chls=1,0,0&chds=#{data.min},#{data.max}"
         )

543bb388afda3d3d580d4e18e86d005a5529f2e4
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2009-01-13 23:52:30 +0800

    testing install.rb
    Signed-off-by: Marco Otte-Witte <marco.otte-witte@simplabs.com>

--- install.rb
+++ install.rb
@@ -1,8 +1,8 @@
-supported_databases = [
+supported_databases = []
 #  'ActiveRecord::ConnectionAdapters::PostgreSQLAdapter',
 #  'ActiveRecord::ConnectionAdapters::MysqlAdapter',
 #  'ActiveRecord::ConnectionAdapters::SQLite3Adapter'
-]
+#]
 
 unless supported_databases.include?(ActiveRecord::Base.connection.class.to_s)
   puts <<-EOT

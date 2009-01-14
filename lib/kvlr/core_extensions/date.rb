b15fdc0f574ec4fb68328b05d0176c890d21deda
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2009-01-14 17:27:55 +0800

    moved DateTime#reporting_period to Date#to_reporting_period
    Signed-off-by: Marco Otte-Witte <marco.otte-witte@simplabs.com>

---
+++ lib/kvlr/core_extensions/date.rb
@@ -0,0 +1,25 @@
+module Kvlr #:nodoc:
+
+  module CoreExtensions #:nodoc:
+
+    module Date
+
+      ::Date.class_eval do
+
+        # Converts the DateTime into a Kvlr::ReportsAsSparkline::ReportingPeriod
+        def to_reporting_period(grouping)
+          if grouping.is_a?(Symbol)
+            grouping = Kvlr::ReportsAsSparkline::Grouping.new(grouping)
+          elsif !grouping.is_a?(Kvlr::ReportsAsSparkline::Grouping)
+            raise ArgumentError.new('grouping must be either an instance of Kvlr::ReportsAsSparkline::Grouping or a symbol.')
+          end
+          Kvlr::ReportsAsSparkline::ReportingPeriod.new(grouping, self)
+        end
+
+      end
+
+    end
+
+  end
+
+end

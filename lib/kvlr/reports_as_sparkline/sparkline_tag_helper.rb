0e966f9804d9bdcbac85e1e0473b697fbb139c43
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2009-01-15 16:54:45 +0100

    renamed AssetTagHelper to SparklineTagHelper; updated docs

--- lib/kvlr/reports_as_sparkline/sparkline_tag_helper.rb
+++ lib/kvlr/reports_as_sparkline/sparkline_tag_helper.rb
@@ -14,15 +14,21 @@ module Kvlr #:nodoc:
       #
       # * <tt>width</tt> - The width of the generated image
       # * <tt>height</tt> - The height of the generated image
-      # * <tt>color</tt> - The base color of the generated image (hex code)
+      # * <tt>line_color</tt> - The line color of the sparkline (hex code)
+      # * <tt>fill_color</tt> - The color to fill the area below the sparkline with (hex code)
+      # * <tt>labes</tt> - The axes to render lables for (Array of :x, :y, :r, :t; this is x axis, y axis, right, top)
       #
       # ==== Example
       # <%= sparkline_tag(User.registrations_report, :width => 200, :height => 100, :color => '000') %>
       def sparkline_tag(data, options = {})
-        options.reverse_merge!({:width => 300, :height => 34, :color => '0077cc'})
-        data.collect! { |d| d[1] }
+        options.reverse_merge!({ :width => 300, :height => 34, :line_color => '0077cc', :fill_color => 'e6f2fa', :labels => [] })
+        data = data.collect { |d| d[1] }
+        labels = ""
+        unless options[:labels].empty?
+          labels = "&chxt=#{options[:labels].map(&:to_s).join(',')}&chxr=0,0,#{data.length}|1,0,#{data.max}|2,0,#{data.max}|3,0,#{data.length}"
+        end
         image_tag(
-          "http://chart.apis.google.com/chart?cht=ls&chs=#{options[:width]}x#{options[:height]}&chd=t:#{data.join(',')}&chco=#{options[:color]}&chm=B,E6F2FA,0,0,0&chls=1,0,0&chds=#{data.min},#{data.max}"
+          "http://chart.apis.google.com/chart?cht=ls&chs=#{options[:width]}x#{options[:height]}&chd=t:#{data.join(',')}&chco=#{options[:line_color]}&chm=B,#{options[:fill_color]},0,0,0&chls=1,0,0&chds=#{data.min},#{data.max}#{labels}"
         )
       end
 

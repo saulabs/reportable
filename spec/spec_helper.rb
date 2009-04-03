33fe1197f6f34ee2f9da7516e818559713fba948
Myron Marston <myron.marston@gmail.com>
2009-04-03 08:14:41 +0800

    Enable debugger if rdebug is installed for specs.
    Signed-off-by: Marco Otte-Witte <marco.otte-witte@simplabs.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,3 +13,11 @@ require File.join(File.dirname(__FILE__), 'boot') unless defined?(ActiveRecord)
 class User < ActiveRecord::Base; end
 
 class YieldMatchException < Exception; end
+
+begin
+  require 'ruby-debug'
+  Debugger.start
+  Debugger.settings[:autoeval] = true if Debugger.respond_to?(:settings)
+rescue LoadError
+  # ruby-debug wasn't available so neither can the debugging be
+end

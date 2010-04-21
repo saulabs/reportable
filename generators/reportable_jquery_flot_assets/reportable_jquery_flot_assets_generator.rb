c4395a017d0ae744f6644be99b17732254380103
Martin Kavalar <martin@sauspiel.de>
2010-04-21 18:29:54 +0200

    Adding ReportableJqueryFlotAssetsGenerator and rename ReportableAssetsGenerator to ReportableGraficoAssetsGenerator

---
+++ generators/reportable_jquery_flot_assets/reportable_jquery_flot_assets_generator.rb
@@ -0,0 +1,42 @@
+if Saulabs::Reportable::IS_RAILS3
+
+  class ReportableJqueryFlotAssetsGenerator < Rails::Generators::Base
+
+    include Rails::Generators::Actions
+
+    def create_grafico_file
+      empty_directory('public/javascripts')
+      copy_file(
+        File.join(File.dirname(__FILE__), 'templates', 'jquery.min.js'),
+        'public/javascripts/jquery.min.js'
+      )
+      copy_file(
+        File.join(File.dirname(__FILE__), 'templates', 'jquery.flot.min.js'),
+        'public/javascripts/jquery.flot.min.js'
+      )
+      copy_file(
+        File.join(File.dirname(__FILE__), 'templates', 'excanvas.min.js'),
+        'public/javascripts/excanvas.min.js'
+      )
+      readme(File.join(File.dirname(__FILE__), 'templates', 'NOTES'))
+    end
+
+  end
+
+else
+
+  class ReportableJqueryFlotAssetsGenerator < Rails::Generator::Base
+
+    def manifest
+      record do |m|
+        m.directory('public/javascripts')
+        m.file('jquery.min.js', 'public/javascripts/jquery.min.js')
+        m.file('jquery.flot.min.js', 'public/javascripts/jquery.flot.min.js')
+        m.file('excanvas.min.js', 'public/javascripts/excanvas.min.js')
+        m.readme('NOTES')
+      end
+    end
+
+  end
+
+end

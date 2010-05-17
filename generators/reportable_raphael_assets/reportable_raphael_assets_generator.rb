74966b44254dcd1b078399152844a053bf27908f
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2010-05-17 15:42:04 +0200

    fixed the generators, added missing JS assets

--- generators/reportable_raphael_assets/reportable_raphael_assets_generator.rb
+++ generators/reportable_raphael_assets/reportable_raphael_assets_generator.rb
@@ -10,6 +10,14 @@ if Saulabs::Reportable::IS_RAILS3
         File.join(File.dirname(__FILE__), 'templates', 'raphael.min.js'),
         'public/javascripts/raphael.min.js'
       )
+      copy_file(
+        File.join(File.dirname(__FILE__), 'templates', 'g.raphael.min.js'),
+        'public/javascripts/g.raphael.min.js'
+      )
+      copy_file(
+        File.join(File.dirname(__FILE__), 'templates', 'g.line.min.js'),
+        'public/javascripts/g.line.min.js'
+      )
       readme(File.join(File.dirname(__FILE__), 'templates', 'NOTES'))
     end
 
@@ -23,6 +31,8 @@ else
       record do |m|
         m.directory('public/javascripts')
         m.file('raphael.min.js', 'public/javascripts/raphael.min.js')
+        m.file('g.raphael.min.js', 'public/javascripts/g.raphael.min.js')
+        m.file('g.line.min.js', 'public/javascripts/g.line.min.js')
         m.readme('NOTES')
       end
     end

96f40a0780ef3c4bd3dcf862da9c9c83e0d632b9
Martin Kavalar <martin@sauspiel.de>
2012-02-14 10:45:01 +0100

    Remove rails 2.x generators

--- generators/reportable_migration/reportable_migration_generator.rb
+++ generators/reportable_migration/reportable_migration_generator.rb
@@ -1,40 +1,20 @@
-if Saulabs::Reportable::RailsAdapter::IS_RAILS3
+class ReportableMigrationGenerator < Rails::Generators::Base
 
-  class ReportableMigrationGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
 
-    include Rails::Generators::Migration
-
-    def create_migration
-      migration_template(
-        File.join(File.dirname(__FILE__), 'templates', 'migration.rb'),
-        'db/migrate/create_reportable_cache.rb'
-      )
-    end
-
-    def self.next_migration_number(dirname)
-      if ActiveRecord::Base.timestamped_migrations
-        Time.now.utc.strftime("%Y%m%d%H%M%S")
-      else
-        "%.3d" % (current_migration_number(dirname) + 1)
-      end
-    end
-
+  def create_migration
+    migration_template(
+      File.join(File.dirname(__FILE__), 'templates', 'migration.rb'),
+      'db/migrate/create_reportable_cache.rb'
+    )
   end
 
-else
-
-  class ReportableMigrationGenerator < Rails::Generator::Base
-
-    def manifest
-      record do |m|
-        m.migration_template('migration.rb', 'db/migrate')
-      end
+  def self.next_migration_number(dirname)
+    if ActiveRecord::Base.timestamped_migrations
+      Time.now.utc.strftime("%Y%m%d%H%M%S")
+    else
+      "%.3d" % (current_migration_number(dirname) + 1)
     end
-
-    def file_name
-      'create_reportable_cache'
-    end
-
   end
 
 end
